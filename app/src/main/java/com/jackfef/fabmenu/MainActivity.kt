d9326653392ea2dd30c934dbff6bfc0fe600c6df
netef <netef.work@gmail.com>
2020-04-12 17:48:55 +0300

    first commit

---
+++ app/src/main/java/com/jackfef/fabmenu/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jackfef.fabmenu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
